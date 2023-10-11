e1478298eddae53eaa7cf0ec639d3c524bce1af0
Mauroiwer <maurodepari06@gmail.com>
2023-10-11 09:42:54 +0200

    terzo commit

---
+++ src/Macchina.java
@@ -0,0 +1,94 @@
+public class Macchina {
+    
+    //attributi
+    private String marca;
+    private String modello;
+    private int codice;
+    private int targa;
+    private int numeroposti;
+
+
+    
+    //metodo costruttore
+    public Macchina(String marca, String modello,int codice,int targa,int numeroposti){
+        this.marca=marca;
+        this.modello=modello;
+        this.codice=codice;
+        this.targa=targa;
+        this.numeroposti=numeroposti;
+
+        
+    }
+    
+    public Macchina(){
+        this.marca="";
+        this.modello="";
+        this.codice=0;
+        this.targa=0;
+        this.numeroposti=0;
+
+    }
+    
+    public Macchina(Macchina that){
+        this.marca=that.marca;
+        this.modello=that.modello;
+        this.codice=that.codice;
+        this.targa=that.targa;
+        this.numeroposti=that.numeroposti;
+
+    }
+    
+    
+    //metodi get  
+    public String getMarca(){
+        return this.marca;
+    }
+    public String getModello(){
+        return this.modello;
+    }
+    public int getCodice(){
+        return this.codice;
+    }
+    public int getTarga(){
+        return this.targa;
+    }
+    public int getNumeroposti(){
+        return this.numeroposti;
+    }
+
+
+    
+    //metodi set   
+    
+    public void setMarca(String marca){
+        this.marca=marca;
+    }
+    public void setModello(String modello){
+        this.modello=modello;
+    }
+    public void setCodice(int codice){
+        this.codice=codice;
+    }
+    public void setTarga(int targa){
+        this.targa=targa;
+    }
+    public void setNumeroposti(int numeroposti){
+        this.numeroposti=numeroposti;
+    }
+    
+    
+        
+        public String toString(){
+        return "Marca "+this.marca+" Modello "+this.modello+" codice "+this.codice+" targa "+this.targa+" numeroposti "+this.numeroposti;
+    }
+    
+    
+    public boolean equals(Macchina that){
+       boolean r=false;
+       if(this.marca==that.marca && this.modello==that.modello && this.codice==that.codice && this.targa==that.targa && this.numeroposti==that.numeroposti){
+           r=true;
+       }
+       return r;
+    }
+
+}
